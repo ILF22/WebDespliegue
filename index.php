9bcbc9a41590d9cbac4452c8644a76bfdecef1c6
ILF22 <2_web9_18@iesjovellanos.org>
2019-02-07 20:16:36 +0100

    Primera version

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php 
+  include("includes/header.inc");
+?>
+<br>
+<br>
+<div class="container-fluid text-center">    
+  <div class="row content">
+    <div class="col-sm-2 sidenav">
+      <p><a href="https://es.wikipedia.org/wiki/Tim_Berners-Lee" target="_blank">Tim Berners-Lee</a></p><br>
+      <p><a href="https://es.wikipedia.org/wiki/James_Gosling" target="_blank">James Gosling</a></p><br>
+      <p><a href="https://es.wikipedia.org/wiki/Guido_van_Rossum" target="_blank">Guido van Rossum</a></p><br>
+    </div>
+    <div class="col-sm-7 text-left"> 
+      <h1>¡Bienvenido!</h1>
+      <p>Esta es una página se explicaran varios lenguajes de marcas. </p>
+      <hr>
+      <h3>Leguajes de marcas</h3>
+      <p>Un lenguaje de marcado o lenguaje de marcas es una 
+      forma de codificar un documento que, junto con el texto, 
+      incorpora etiquetas o marcas que contienen información 
+      adicional acerca de la estructura del texto o su presentación.</p>
+      <div class= "pp">
+        <img src="img/pp.jpg">
+      </div>
+    </div>
+    <div class="col-sm-3 sidenav">
+      <div>
+        <iframe src="https://www.youtube.com/embed/wq4S6BlIFmk" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      </div>
+      <div >
+        <img class="imgL" src="img/lenguajes.png">
+      </div>
+    </div>
+  </div>
+</div>
+<?php 
+  include("includes/footer.inc");
+?>
+</body>
+</html>
---
+++ java.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php 
+  include("includes/header.inc");
+?>
+<br>
+<br>
+<div class="container-fluid text-center">    
+  <div class="row content">
+  <div class="col-sm-2 sidenav">
+      <p><a href="https://es.wikipedia.org/wiki/Tim_Berners-Lee" target="_blank">Tim Berners-Lee</a></p><br>
+      <p><a href="https://es.wikipedia.org/wiki/James_Gosling" target="_blank">James Gosling</a></p><br>
+      <p><a href="https://es.wikipedia.org/wiki/Guido_van_Rossum" target="_blank">Guido van Rossum</a></p><br>
+    </div>
+    <div class="col-sm-8 text-left"> 
+      <h1>Welcome</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+      <hr>
+      <h3>Test</h3>
+      <p>Lorem ipsum...</p>
+    </div>
+    <div class="col-sm-3 sidenav">
+      <div>
+        <iframe src="https://www.youtube.com/embed/wq4S6BlIFmk" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      </div>
+      <div >
+        <img class="imgL" src="img/lenguajes.png">
+      </div>
+    </div>
+  </div>
+</div>
+<?php 
+  include("includes/footer.inc");
+?>
+</body>
+</html>
